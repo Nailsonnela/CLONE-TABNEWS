6d67c9e953a80fc559616b2e69c67648b645e46b
Nailsonnela <nailsonnela048@gmail.com>
2024-12-11 23:51:01 +0000

    novo arquivo pages

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+function home() {
+  return (
+    <h1>
+      {" "}
+      meu amor dessa vez essa mensagen nao e pra alguen so pra provar que eu
+      estou estudando,ela e diretamnente pra voce MY AMOR,so voce pode ve-la, eu
+      to aqui pra dizer que eu TE AMO muito e dizer que eu nao to fazendo
+      programacao pra me no futuro,eu to fazendo por EU E VOCE ou melhor por NOS
+      DOIS, eu quero poder lhe prorcionar uma vida melhor mais pra isso tenho
+      que esta pronto para quando a oportunidade chegar eu segurar firme e nao
+      solta-la mais, espero que tenha paciencia pois serao tempos de muito
+      apredizados para nos dois,eu nao quero deixar essa oportunidade passar,EU
+      TE AMO MUITO E QUERO VOCE NA MINHA VIDA HOJE,AMANHA E SEMPRE!!!
+    </h1>
+  );
+}
+
+export default home;
