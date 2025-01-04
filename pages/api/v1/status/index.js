c7911fd283388a1edfbc91a956c99601c01c2a74
Nailsonnela <nailsonnela048@gmail.com>
2025-01-04 20:39:34 +0000

    add `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,8 +1,4 @@
-import database from "infra/database.js";
-
-async function status(requere, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
+function status(request, response) {
   response.status(200).json({ chave: "valor" });
 }
 
